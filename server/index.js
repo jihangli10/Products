118e83d88ff69532be01613cefa27c56e9320192
Jihang Li <lijihang26@gmail.com>
2021-03-23 11:51:02 -0400

    Implemented interactions and cart

--- server/index.js
+++ server/index.js
@@ -3,103 +3,25 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const Mongo = require('../database');
+const dummyData = require('./data.js');
 const app = express();
 
 // Middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const reviews = [
-  {
-    "review_id": 234310,
-    "rating": 5,
-    "summary": "Autem error eius ut ipsa accusantium.",
-    "recommend": false,
-    "response": null,
-    "body": "Distinctio aperiam ut iure et quibusdam suscipit officiis. Quis et et. Sint porro quasi dolore sapiente aspernatur. Blanditiis quo sed. Velit quis et harum consequatur. Distinctio aperiam ut iure et quibusdam suscipit officiis. Quis et et. Sint porro quasi dolore sapiente aspernatur. Blanditiis quo sed. Velit quis et harum consequatur. Distinctio aperiam ut iure et quibusdam suscipit officiis. Quis et et. Sint porro quasi dolore sapiente aspernatur. Blanditiis quo sed. Velit quis et harum consequatur. ",
-    "date": "2020-09-09T00:00:00.000Z",
-    "reviewer_name": "Floy_Lubowitz79",
-    "helpfulness": 16,
-    "photos": [
-      {
-        "id": 405472,
-        "url": "https://images.unsplash.com/photo-1507920676663-3b72429774ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
-      }
-    ]
-  },
-  {
-    "review_id": 234312,
-    "rating": 5,
-    "summary": "Quisquam assumenda illum sunt voluptas.",
-    "recommend": true,
-    "response": null,
-    "body": "Neque sed quibusdam quia et qui dolores. Ut praesentium eos et voluptas. Explicabo dolores aut et.",
-    "date": "2020-10-10T00:00:00.000Z",
-    "reviewer_name": "Malachi74",
-    "helpfulness": 6,
-    "photos": [
-      {
-        "id": 405468,
-        "url": "https://images.unsplash.com/photo-1447879027584-9d17c2ca0333?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80"
-      },
-      {
-        "id": 405469,
-        "url": "https://images.unsplash.com/photo-1526948128573-703ee1aeb6fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-      },
-      {
-        "id": 405470,
-        "url": "https://images.unsplash.com/photo-1532244769164-ff64ddeefa45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-      }
-    ]
-  },
-  {
-    "review_id": 234308,
-    "rating": 4,
-    "summary": "Sunt et ullam molestias et recusandae est assumenda accusamus.",
-    "recommend": true,
-    "response": null,
-    "body": "Distinctio aperiam ut iure et quibusdam suscipit officiis. Quis et et. Sint porro quasi dolore sapiente aspernatur. Blanditiis quo sed. Velit quis et harum consequatur. Distinctio aperiam ut iure et quibusdam suscipit officiis. Quis et et. Sint porro quasi dolore sapiente aspernatur. Blanditiis quo sed. Velit quis et harum consequatur. Distinctio aperiam ut iure et quibusdam suscipit officiis. Quis et et. Sint porro quasi dolore sapiente aspernatur. Blanditiis quo sed. Velit quis et harum consequatur. ",
-    "date": "2020-05-29T00:00:00.000Z",
-    "reviewer_name": "Robyn64",
-    "helpfulness": 1,
-    "photos": [
-      {
-        "id": 405475,
-        "url": "https://images.unsplash.com/photo-1515243061678-14fc18b93935?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-      },
-      {
-        "id": 405476,
-        "url": "https://images.unsplash.com/photo-1530821875964-91927b611bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-      },
-      {
-        "id": 405477,
-        "url": "https://images.unsplash.com/photo-1511766566737-1740d1da79be?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-      }
-    ]
-  }
-];
 
-const cart = [
-  {
-      "sku_id": 1,
-      "count": 2
-  },
-  {
-      "sku_id": 3,
-      "count": 1
-  },
-  {
-      "sku_id": 5,
-      "count": 33
-  }
-]
 
 // Connect to server
 var Products;
+var Interactions;
+var Cart;
 var size;
 Mongo.connectToServer(async () => {
   db = await Mongo.getDB();
   Products = db.collection('products');
+  Interactions = db.collection('interactions');
+  Cart = db.collection('cart');
   size = await Products.countDocuments({});
 });
 
@@ -115,7 +37,7 @@ app.get('/products', async (req, res) => {
     { $project: {_id: 0, id: "$_id", name: 1, slogan: 1, description: 1, category: 1, default_price: 1, created_at: 1, updated_at: 1} }
   ]);
   result.toArray((err, docs) => {
-  res.send(docs);
+  res.status(200).send(docs);
   });
 });
 
@@ -126,7 +48,7 @@ app.get('/products/:product_id', async (req, res) => {
     { $project: {_id: 0, id: "$_id", name: 1, slogan: 1, description: 1, category: 1, default_price: 1, created_at: 1, updated_at: 1, features: 1} }
   ]);
   result.toArray((err, docs) => {
-  res.send(docs[0]);
+  res.status(200).send(docs[0]);
   });
 });
 
@@ -137,7 +59,7 @@ app.get('/products/:product_id/styles', async (req, res) => {
     { $project: {_id: 0, product_id: "$_id", results: "$styles"} }
   ]);
   result.toArray((err, docs) => {
-  res.send(docs[0]);
+  res.status(200).send(docs[0]);
   });
 })
 
@@ -148,18 +70,48 @@ app.get('/products/:product_id/related', async (req, res) => {
     { $project: {related: 1} }
   ]);
   result.toArray((err, docs) => {
-  res.send(docs[0].related);
+  res.status(200).send(docs[0].related);
+  });
+});
+
+// Interactions
+app.post('/interactions', async (req, res) => {
+  await Interactions.insertOne(req.body);
+  res.status(201).send('Created');
+});
+
+// Cart
+
+app.post('/cart', async (req, res) => {
+  var result = await Cart.find({sku_id: req.body.sku_id});
+  result.toArray(async (err, docs) => {
+    if (docs.length === 0) {
+      await Cart.insertOne({sku_id: req.body.sku_id, count: 1});
+      res.status(201).send('Created');
+    } else {
+      await Cart.update({sku_id: req.body.sku_id},  {$inc: { count: 1 } });
+      res.status(201).send('Created');
+    }
+  });
+});
+
+app.get('/cart', async (req, res) => {
+  var result = await Cart.find({}, { sku_id: 1, count: 1, _id: 0 });
+  result.toArray((err, docs) => {
+    res.status(200).send(docs);
   });
 })
 
-app.get('/reviews', async (req, res) => {
-  res.send({results: reviews});
+// Dummy data routers
+app.get('/reviews', (req, res) => {
+  res.send({results: dummyData.reviews});
 })
 
-app.get('/cart', async (req, res) => {
-  res.send({results: cart});
+app.get('/reviews/meta', (req, res) => {
+  res.send({results: dummyData.meta});
 })
 
+// Hello world!
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
